b468b95b6da151baa52dc2c8992f456a527e4a94
Chinmay Nerurkar <nchinmay@hotmail.com>
2013-04-09 16:28:42 -0400

    SenderTester changes

--- test/panda/tester/SenderTester.java
+++ test/panda/tester/SenderTester.java
@@ -30,69 +30,6 @@ public class SenderTester
 		return b;
 	}
 
-	/*
-	 * public void stopSender() { this.stopSender = true; }
-	 * 
-	 * // 24 <= dataSize <= Utils.MAX_MESSAGE_PAYLOAD_SIZE public void
-	 * SendSequencedMessages(int cacheSize, TopicInfo tInfo, int dataSize, final
-	 * long numOfMessages, int netRecvBufferSize) throws Exception { final
-	 * PandaAdapter adapter = new PandaAdapter(cacheSize); final TopicInfo
-	 * topicInfo = tInfo; long seqNumber = 0; long mssgPerSecCounter = 0;
-	 * 
-	 * long currentTimeStamp = 0; long startSendTimeStamp =
-	 * System.currentTimeMillis(); long secondCounter = startSendTimeStamp;
-	 * 
-	 * adapter.subscribe(topicInfo, InetAddress.getLocalHost().getHostAddress(),
-	 * new IDataListener() { private long messageSeqNum; private int
-	 * returnedMessageCount; private long startedRecvTimeStamp; private long
-	 * recdThisSecondTimeStamp; private long currentTime; private int
-	 * messagesTillLastSec; private long endRecvTimeStamp;
-	 * 
-	 * @Override public void receivedPandaData(int topicId, ByteBuffer payload)
-	 * { this.messageSeqNum = payload.getLong(8); ++this.returnedMessageCount;
-	 * 
-	 * if (this.startedRecvTimeStamp == 0) { this.startedRecvTimeStamp =
-	 * System.currentTimeMillis(); this.recdThisSecondTimeStamp =
-	 * this.startedRecvTimeStamp; }
-	 * 
-	 * if ((this.currentTime = System.currentTimeMillis()) >
-	 * this.recdThisSecondTimeStamp + 1000) { this.recdThisSecondTimeStamp =
-	 * this.currentTime; System.out.println("--- Recd. " +
-	 * (this.returnedMessageCount - this.messagesTillLastSec) +
-	 * " messages in the last second. Total Messages Recd. " +
-	 * this.returnedMessageCount); this.messagesTillLastSec =
-	 * this.returnedMessageCount; }
-	 * 
-	 * if (this.returnedMessageCount == numOfMessages) { this.endRecvTimeStamp =
-	 * System.currentTimeMillis(); System.out.println("*** Recd. " +
-	 * this.returnedMessageCount + " in " + (this.endRecvTimeStamp -
-	 * this.startedRecvTimeStamp) + " milliseconds. Recd. sender seq # " +
-	 * this.messageSeqNum); System.out.println("*** Messages Lost = " +
-	 * (this.messageSeqNum - numOfMessages) + " (" + (100 * (float)
-	 * (this.messageSeqNum - numOfMessages) / this.messageSeqNum) + " %)");
-	 * SenderTester.this.stopSender(); } } }, netRecvBufferSize);
-	 * 
-	 * while (true ) { if (this.stopSender) {
-	 * System.out.println("*** Last Message Sent By Original Sender Seq. # " +
-	 * seqNumber); System.exit(0); } ByteBuffer buffer =
-	 * ByteBuffer.allocate(dataSize); buffer.putLong(0,
-	 * this.senderInitTimeStamp); // for Sender // Identification at // app
-	 * level buffer.putLong(8, ++seqNumber); buffer.putLong(16,
-	 * System.currentTimeMillis());
-	 * 
-	 * if ((currentTimeStamp = System.currentTimeMillis()) > secondCounter +
-	 * 1000) { secondCounter = currentTimeStamp; System.out.println("--- Sent "
-	 * + (seqNumber - mssgPerSecCounter) +
-	 * " messages this second. Total messages sent " + seqNumber);
-	 * mssgPerSecCounter = seqNumber; } adapter.send(topicInfo,
-	 * InetAddress.getLocalHost().getHostAddress(), buffer.array()); if
-	 * (seqNumber % 400 == 0) { Thread.sleep(1); } } //
-	 * System.out.println("Sent " + numOfMessages + " messages in " + //
-	 * (System.currentTimeMillis() - startSendTimeStamp) + " milliseconds");
-	 * 
-	 * }
-	 */
-
 	long messageSeqNum;
 	long highestSeqNum;
 	int returnedMessageCount;
