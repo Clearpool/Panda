44c860add3432f8fb3f4fdffaca35ca21d9f5e3b
Parag Patel <Parag.Patel@fusionts.com>
2014-03-27 19:31:07 -0400

    CLEAR-1791 .. fix bug

--- src/com/clearpool/panda/core/ChannelSendInfo.java
+++ src/com/clearpool/panda/core/ChannelSendInfo.java
@@ -150,7 +150,7 @@ class ChannelSendInfo implements SelectorActionable
 		while (hasOutboundDataRemaining())
 		{
 			byte[] bytes = getNextPacket();
-			if (this.sequenceNumber % 5L == 0L) return;
+			//if (this.sequenceNumber % 5L == 0L) return;
 			this.channel.setOption(StandardSocketOptions.IP_MULTICAST_IF, this.networkInterface);
 			this.channel.setOption(StandardSocketOptions.IP_MULTICAST_TTL, Integer.valueOf(255));
 			this.channel.send(ByteBuffer.wrap(bytes), this.multicastGroupAddress);
