85bf3a76d20940033e2a661c9c65ff6803adf6c5
Nachiket Joshi <njoshi@clearpoolgroup.com>
2014-05-13 17:09:30 -0400

    CLEAR-1999: Updated SendTest

--- test/com/clearpool/panda/utils/SendTest.java
+++ test/com/clearpool/panda/utils/SendTest.java
@@ -2,38 +2,56 @@ package com.clearpool.panda.utils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Date;
 
 import com.clearpool.panda.core.PandaAdapter;
 import com.clearpool.panda.core.PandaUtils;
 
-
 public class SendTest
 {
-	private static final String TOPIC1 = "ONE";
+	private static final String TOPIC = "ONE";
 	private static final String IP = "239.9.9.10";
 	private static final int PORT = 9002;
-	private static final String MULTICASTGROUP = PandaUtils.getMulticastGroup(IP, PORT);
-	
+	static final String MULTICASTGROUP = PandaUtils.getMulticastGroup(IP, PORT);
+	static final String LOCAL_IP = getLocalIp(null);
+
 	public static void main(String[] args) throws Exception
 	{
 		final PandaAdapter adapter = new PandaAdapter(1000);
-		final String localIp = getLocalIp(null);
-
-		int count = 0;
-		while (true)
+		for (int n = 0; n < 5; n++)
 		{
-			for (int i = 0; i < 500; i++)
+			long start = System.currentTimeMillis();
+			SendTester s1 = new SendTester(adapter);
+			s1.start();
+			s1.join();
+			System.out.println("Test took " + (System.currentTimeMillis() - start) + " millis");
+			Thread.sleep(2000);
+		}
+		System.exit(0);
+	}
+
+	static class SendTester extends Thread
+	{
+		private final PandaAdapter adapter;
+
+		public SendTester(PandaAdapter adapter)
+		{
+			this.adapter = adapter;
+		}
+
+		@Override
+		public void run()
+		{
+			for (int i = 0; i < 1000000; i++)
 			{
-				count++;
-				String countString = String.valueOf(count);
-				adapter.send(TOPIC1, IP, PORT, MULTICASTGROUP, localIp, countString.getBytes());
-				if (count % 100000 == 0)
+				try
 				{
-					System.out.println(new Date());
+					this.adapter.send(TOPIC, IP, PORT, MULTICASTGROUP, LOCAL_IP, String.valueOf(i).getBytes());
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
 				}
 			}
-			Thread.sleep(1);
 		}
 	}
 
