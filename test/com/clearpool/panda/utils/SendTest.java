f11aa0bc9f634583145effa4be0734b5568fedf9
Nachiket Joshi <njoshi@clearpoolgroup.com>
2014-05-14 14:12:17 -0400

    CLEAR-1999: Updated SendTest

--- test/com/clearpool/panda/utils/SendTest.java
+++ test/com/clearpool/panda/utils/SendTest.java
@@ -2,54 +2,47 @@ package com.clearpool.panda.utils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Date;
 
 import com.clearpool.panda.core.PandaAdapter;
-import com.clearpool.panda.core.PandaUtils;
-
 
 public class SendTest
 {
-	private static final String TOPIC1 = "ONE";
+	private static final String TOPIC = "ONE";
 	private static final String IP = "239.9.9.10";
 	private static final int PORT = 9002;
-	private static final String MULTICASTGROUP = PandaUtils.getMulticastGroup(IP, PORT);
-	
+	private static final String MULTICASTGROUP = IP + ":" + PORT;
+	private static final String LOCAL_IP = getLocalIp();
+
 	public static void main(String[] args) throws Exception
 	{
 		final PandaAdapter adapter = new PandaAdapter(1000);
-		final String localIp = getLocalIp(null);
-
 		int count = 0;
+		long start = System.currentTimeMillis();
+
 		while (true)
 		{
-			for (int i = 0; i < 500; i++)
+			count++;
+			String countString = String.valueOf(count);
+			adapter.send(TOPIC, IP, PORT, MULTICASTGROUP, LOCAL_IP, countString.getBytes());
+			if (count % 100000 == 0)
 			{
-				count++;
-				String countString = String.valueOf(count);
-				adapter.send(TOPIC1, IP, PORT, MULTICASTGROUP, localIp, countString.getBytes());
-				if (count % 100000 == 0)
-				{
-					System.out.println(new Date());
-				}
+				long end = System.currentTimeMillis();
+				System.out.println("Took " + (end - start) + " milliseconds to send 100000 messages to multicast");
+				start = end;
 			}
-			Thread.sleep(1);
+			if (count % 500 == 0) Thread.sleep(1);
 		}
 	}
 
-	private static String getLocalIp(String ip)
+	private static String getLocalIp()
 	{
-		if (ip == null)
+		try
 		{
-			try
-			{
-				return InetAddress.getLocalHost().getHostAddress();
-			}
-			catch (UnknownHostException e)
-			{
-				return null;
-			}
+			return InetAddress.getLocalHost().getHostAddress();
 		}
-		return ip;
+		catch (UnknownHostException e)
+		{
+			return null;
+		}
 	}
 }
