77d008135060795ce24d6cacc588eb0480e1fd06
parag.patel <ppatel@clearpoolgroup.com>
2014-06-27 18:07:00 -0400

    CLEAR-2333 .. open source panda

--- src/com/clearpool/panda/core/PandaDataListener.java
+++
@@ -1,9 +0,0 @@
-package com.clearpool.panda.core;
-
-import java.nio.ByteBuffer;
-
-public interface PandaDataListener
-{
-	void receivedPandaData(String topic, ByteBuffer payload);
-	void receivedPandaError(PandaErrorCode issueCode, String multicastGroup, String message, Throwable throwable);
-}
